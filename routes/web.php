53ffd703a96d4ec455bb6be6280ee8d61adac40d
Gonzalo Zevallos <gonzalo.zev@gmail.com>
2019-05-26 17:08:20 -0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
+Route::get('/','PageController@MostrarPreHome');
 Route::get('/login','PageController@MostrarLogin');
 Route::get('/register','PageController@MostrarRegister');
-Route::get('/index','PageController@MostrarPreHome');
 Route::get('/home','PageController@MostrarHome');
 Route::get('/FAQs','PageController@MostrarFaqs');
 
